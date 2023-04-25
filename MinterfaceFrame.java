aa068f7f206f26cb8d351f034216cc68a3eea3cb
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-24 20:53:41 -0500

    Update MinterfaceFrame.java

--- MinterfaceFrame.java
+++ MinterfaceFrame.java
@@ -1,21 +1,29 @@
 package Minter;
 
 import java.awt.FlowLayout;
+import javax.swing.JFrame;
 
 //*//*
 public class MinterfaceFrame extends Bankaccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				MinterfaceGUI FrameObject = new MinterfaceGUI();//Creates ColoringFrame object
 				
-				// SECOND FRAME TEST INITIALIZE
-				MinterfaceTestFrame FrameTest = new MinterfaceTestFrame();
+		// LOG-IN FRAME
+		MinterfaceGUI FrameObject = new MinterfaceGUI();//Creates ColoringFrame object
+		
+		// USER DASHBOARD FRAME
+		//MinterfaceUserFrame FrameTest = new MinterfaceUserFrame();
+		
+		// USER SETTINGS & PREFERENCES FRAME
+		
+		// ADMIN MENU FRAME
+		
+		// 
 				
-				//FrameObject.setDefaultCloseOperation(EXIT_ON_CLOSE); //Closes Application upon close
-				FrameObject.setSize(684,412); //Sets size to 300 by 300
-				FrameObject.setVisible(true); //Set as visible
-				
+		FrameObject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Closes Application upon close
+		FrameObject.setSize(684,412); //Sets size to 300 by 300
+		FrameObject.setVisible(true); //Set as visible
 				
 	}			
 }
