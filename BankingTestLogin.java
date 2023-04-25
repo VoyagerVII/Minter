076775d84df39b75c9deadd31078186abf948e80
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 15:50:04 -0500

    Update BankingTestLogin.java

--- BankingTestLogin.java
+++ BankingTestLogin.java
@@ -1,3 +1,8 @@
+//BankingTestLogin.java
+//04/25/2023
+//Tests to see if the user can login correctly 
+
+
 package Minter;
 
 import static org.junit.Assert.*;
@@ -6,9 +11,10 @@ import org.junit.Test;
 
 public class BankingTestLogin extends Bankaccount {
 	Bankaccount test = new Bankaccount();
-
+//runs the login test
 	@Test
 	public void test() {
+		//tests using the information provided
 		System.out.printf("Test 3: Running Login Test");
 		test.Account("Chrismedr", "cmedrano6@islander", "12PWQE34**", "Christian Medrano");
 		System.out.printf("Login Test Result : ");
@@ -19,3 +25,4 @@ public class BankingTestLogin extends Bankaccount {
 	
 
 }
+
