b518ff72afa741e4b0100392d4528ae27def21d1
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 15:41:24 -0500

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,18 +1,22 @@
+//Account.java
+//04/25/2023
+//This will store the account information
 package Minter;
-
+//helps the user to create their bank account
 public class Account extends Bankaccount {
 	private String username;
     private String email;
     private String password;
     private String fullName;
-
+//stores the username, email,password, and name
     public Account(String username, String email, String password, String fullName) {
-        this.username = username;
+    	this.username = username;
         this.email = email;
         this.password = password;
         this.fullName = fullName;
+
     }
-  
+  //checks to see if the log in information matches anything stored
     public boolean login(String username, String password) {
         if (this.username.equals(username) && this.password.equals(password)) {
             return true;
@@ -20,36 +24,44 @@ public class Account extends Bankaccount {
         	return false;
         }
     }
-
+//gets the username
     public String getUsername() {
         return username;
     }
-
+//gets the email
     public String getEmail() {
         return email;
     }
-
+//gets the password
     public String getPassword() {
         return password;
     }
-
+//gets the full name
     public String getFullName() {
         return fullName;
     }
-
+// sets the username
     public void setUsername(String username) {
-        this.username = username;
+    	if (username==null)
+        {
+        	throw new IllegalArgumentException ("Input must be valid");
+        }
+    	this.username = username;
     }
-
+//sets the email for the account
     public void setEmail(String email) {
-        this.email = email;
+        if (email==null)
+        {
+        	throw new IllegalArgumentException ("Input must be valid");
+        }
+    	this.email = email;
     }
-
+//sets the password for the account
     public void setPassword(String password) {
-        this.password = password;
-    }
+        if (password==null)
+        {
+        	throw new IllegalArgumentException ("Must be a valid input");
+        }
+    	this.password = password;
+    }}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-}
