a67d57d634f33648f32808a96e5b939fc5cb84d4
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 15:54:34 -0500

    Update MinterfaceUserFrame.java

--- MinterfaceUserFrame.java
+++ MinterfaceUserFrame.java
@@ -1,3 +1,6 @@
+//MinterfaceUserFrame.java
+//04/25/2023
+//helps to display the user frame
 package Minter;
 
 import javax.swing.*;
@@ -10,16 +13,21 @@ public class MinterfaceUserFrame extends JFrame implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField usernameField;
+	
+    private JTextField usernameField;
     private JTextField emailField;
     private JTextField passwordField;
     private JTextField fullNameField;
+    
     private JButton createButton;
+    
+    private Bankaccount bankuserAccount;
 
-   MinterfaceUserFrame() {
+   MinterfaceUserFrame(final Bankaccount bankuser) {
         super("Create Account");
-
+        
         // create the components
+        bankuserAccount = new Bankaccount();
         JLabel usernameLabel = new JLabel("Username:");
         JLabel emailLabel = new JLabel("Email:");
         JLabel passwordLabel = new JLabel("Password:");
@@ -29,7 +37,7 @@ public class MinterfaceUserFrame extends JFrame implements ActionListener {
         passwordField = new JTextField(20);
         fullNameField = new JTextField(20);
         createButton = new JButton("Create");
-
+        
         // add the components to the frame
         JPanel panel = new JPanel(new GridLayout(5, 2));
         panel.add(usernameLabel);
@@ -41,32 +49,41 @@ public class MinterfaceUserFrame extends JFrame implements ActionListener {
         panel.add(fullNameLabel);
         panel.add(fullNameField);
         panel.add(createButton);
-
         add(panel);
 
         // register the button for events
-        createButton.addActionListener(this);
-
         // set the frame properties
+        createButton.addActionListener(new ActionListener() {
+        	
+			public void actionPerformed(ActionEvent e) {
+				 String username = usernameField.getText();
+			        String email = emailField.getText();
+			        String password = passwordField.getText();
+			        String fullName = fullNameField.getText();
+			        bankuser.Account(username, email, password, fullName);
+			        // show a message to the user that the account was created successfully
+			        
+								   try {
+				        bankuser.Account(bankuser.UserInfo.getUsername(), bankuser.UserInfo.getEmail(),
+				        		bankuser.UserInfo.getPassword(),bankuser.UserInfo.getFullName());
+				        } catch(java.lang.NullPointerException d )
+				        {
+				        	bankuser.Account(",",",",",",",");        }
+								   
+					if(bankuser.UserInfo.getPassword()==password&&bankuser.UserInfo.getUsername()==username)
+						JOptionPane.showMessageDialog(createButton, "Account created successfully");
+								        // close the frame
+			dispose();
+			}
+        	
+        });
         setSize(300, 200);
         setVisible(true);
-    }
+        
+   }
 
-    @Override
     public void actionPerformed(ActionEvent e) {
         // create the account with the provided information
-        String username = usernameField.getText();
-        String email = emailField.getText();
-        String password = passwordField.getText();
-        String fullName = fullNameField.getText();
-
-        Bankaccount bankAccount = new Bankaccount();
-        bankAccount.Account(username, email, password, fullName);
-
-        // show a message to the user that the account was created successfully
-        JOptionPane.showMessageDialog(this, "Account created successfully");
-
-        // close the frame
-        dispose();
+       //
     }
 }
