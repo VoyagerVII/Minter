07eb20157462a9cdea797df76fb014d8a93b1a2f
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 15:44:43 -0500

    Update Bankaccount.java

--- Bankaccount.java
+++ Bankaccount.java
@@ -1,20 +1,33 @@
+//Bankaccount.java
+//04/25/2023
+//Helps authenticate the bank account by checking the user account validity
 package Minter;
 
+import javax.naming.AuthenticationException;
+
+//private members allow the class to use these variables
 public class Bankaccount {
 	protected Account UserInfo;
 	protected Balance UserBalance;
-	protected Authentication UserAuth;
-
+	protected AuthenticationException UserAuth;
 public Bankaccount()
-{}
+{
+	
+
+}
+
+//sets the current users balance according to his deposit withdrawal spending and current balance
 void Balance(double deposit, double withdrawal, double spending, double balance) {
     this.UserBalance = new Balance(deposit,withdrawal,spending,balance);
 }
+
+//allow current user to have their information stored
 void Account(String username, String email, String password, String fullName) {
     this.UserInfo = new Account (username,email,password,fullName);
 }
+
+//allows user authenticator to be for the selected user
 void Authentication(String username, String email, String password, String fullName,String key) {
-	this.UserAuth = new Authentication(username,email,password,fullName,key);
+	this.UserAuth = new AuthenticationException(username);
 }
 } 
- 
