745acf4d909d57889c291e89b0427aeff5636237
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 16:49:08 -0500

    Add files via upload

---
+++ MinterfaceDashboardFrame.java
@@ -0,0 +1,52 @@
+package Minter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MinterfaceDashboardFrame extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// TRANSACTIONS
+     private final JPanel TransactionsLabel;
+     private final JRadioButton AccountWithdrawalJRadioButton;
+     private final JRadioButton AccountDepositJRadioButton;
+     private final ButtonGroup AccountTransactions;
+     
+     private final JTextField Amount;
+     
+     public MinterfaceDashboardFrame() {
+    	 
+    	 
+    	 AccountWithdrawalJRadioButton = new JRadioButton("Withdrawal");
+         AccountDepositJRadioButton = new JRadioButton("Deposit"); 
+         TransactionsLabel = new JPanel();
+         
+         Amount = new JTextField(8);
+         
+         TransactionsLabel.add(AccountWithdrawalJRadioButton);
+         TransactionsLabel.add(AccountDepositJRadioButton);
+         TransactionsLabel.add(Amount);
+         
+         AccountTransactions = new ButtonGroup();
+         AccountTransactions.add(AccountWithdrawalJRadioButton);
+         AccountTransactions.add(AccountDepositJRadioButton);
+    	 
+         
+         add(TransactionsLabel);
+     }
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
