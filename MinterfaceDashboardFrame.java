cb3f3baf92a13e133ebadf2d9272a1defdf87b00
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 20:12:07 -0500

    Update MinterfaceDashboardFrame.java

--- MinterfaceDashboardFrame.java
+++ MinterfaceDashboardFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MinterfaceDashboardFrame extends JFrame implements ActionListener {
 
@@ -12,35 +13,49 @@ public class MinterfaceDashboardFrame extends JFrame implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
+	private final ArrayList<String> transactionHistory;
 	
 	// TRANSACTIONS
      private final JPanel TransactionsLabel;
      private final JPanel ConfirmationLabel;
+     private final JPanel UserChoice;
+     
      private final JRadioButton AccountWithdrawalJRadioButton;
      private final JRadioButton AccountDepositJRadioButton;
+     
      private final ButtonGroup AccountTransactions;
+     
      private final JButton Confirmation;
+     private final JButton Logout;
+     
      private final JTextField Amount;
+     
      private final JLabel Transactions;
      
      public MinterfaceDashboardFrame(final Bankaccount bankuser) {
     	 
     	 super("User Dashboard");
     	 
-    	 
+    	 transactionHistory = new ArrayList<>();
     	 
          Confirmation = new JButton("Confirm");
+         Logout = new JButton("Logout");
+         
     	 AccountWithdrawalJRadioButton = new JRadioButton("Withdrawal");
          AccountDepositJRadioButton = new JRadioButton("Deposit"); 
+         
          TransactionsLabel = new JPanel();
          ConfirmationLabel = new JPanel();
+         UserChoice = new JPanel();
+         
          Amount = new JTextField(20);
-         Transactions = new JLabel("Bank Account Transactions Log: \n"
-         		+ ":Balance "+bankuser.UserBalance.getBalance()
-         		+ "\n:Deposits "+bankuser.UserBalance.getDeposit()
-         		+ "\n:Withdraws "+bankuser.UserBalance.getWithdrawal()
-        		 +"\n");
+         
+         Transactions = new JLabel("<html>Bank Account Transactions Log: <br>"
+         		+ "<html>Balance: "+bankuser.UserBalance.getBalance() + "<html><br>"
+         		+ "<html>Deposits: "+bankuser.UserBalance.getDeposit() + "<html><br>"
+         		+ "<html>Withdraws: " +bankuser.UserBalance.getWithdrawal() + "<html><br>"
+        		 +"<html><br>");
+         
          TransactionsLabel.add(AccountWithdrawalJRadioButton);
          TransactionsLabel.add(AccountDepositJRadioButton);
          TransactionsLabel.add(Amount);
@@ -54,17 +69,37 @@ public class MinterfaceDashboardFrame extends JFrame implements ActionListener {
          ConfirmationLabel.add(Transactions);
          add(ConfirmationLabel);
          
+         UserChoice.add(Logout);
+         Logout.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			
+				dispose();
+			}
+
+        	 
+         });
+         
+         add(UserChoice);
+         
+         
          Confirmation.addActionListener(new ActionListener() {
          	
  			public void actionPerformed(ActionEvent e) {
  				
  				if(e.getSource()==Confirmation)
  				{
+ 					String transactionDetails = "";
+ 					
  					if(AccountWithdrawalJRadioButton.isSelected())
  					{
  		 				String Withdrawal = Amount.getText();
  		 		        double amountDouble = Double.parseDouble(Withdrawal);
- 		 				bankuser.UserBalance.setWithdrawal(amountDouble);
+ 		 		        
+ 		 		        transactionDetails = "Withdrawal: " + amountDouble;
+ 		 				
+ 		 		        bankuser.UserBalance.setWithdrawal(amountDouble);
 						JOptionPane.showMessageDialog(Confirmation, "Deposited successfully: "+"Withdrawal: "+ bankuser.UserBalance.getWithdrawal() );
 
  					}	
@@ -73,22 +108,21 @@ public class MinterfaceDashboardFrame extends JFrame implements ActionListener {
  						
  		 				String Deposit = Amount.getText();
  		 		        double amountDouble = Double.parseDouble(Deposit);
+ 		 		        
+ 		 		        transactionDetails = "Deposit: " + amountDouble;
+ 		 		        
  		 				bankuser.UserBalance.setDeposit(amountDouble);
 						JOptionPane.showMessageDialog(Confirmation, "Deposited successfully: "+"Deposit: "+ bankuser.UserBalance.getDeposit() );
 						 
 
  					}
-
+ 						
+ 					transactionHistory.add(transactionDetails);
  				}
- 				
- 				
- 				
- 			dispose();
- 			}
+ 			}			
          	
          });
-         
-         
+
          setSize(684,412);
          setVisible(true);
      }
