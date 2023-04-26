7ca5b1e986fc45847908f1ac9c8014e9010e1f83
VoyagerVII <99273472+VoyagerVII@users.noreply.github.com>
2023-04-25 23:12:01 -0500

    Update BankingTestAuthentication.java

--- BankingTestAuthentication.java
+++ BankingTestAuthentication.java
@@ -6,24 +6,19 @@ import org.junit.Test;
 
 public class BankingTestAuthentication extends Bankaccount {
 	
-	Bankaccount test = new Bankaccount();
+	Bankaccount test4 = new Bankaccount();
 	
 	@Test
 	public void test() {
 		
 		System.out.printf("Test 4: Running Authentication class Test");
-		test.Account("Cmedrano6", "cm6693916", "123Password**","Chrisian Medrano");
+		test4.Account("Cmedrano6", "cm6693916", "123Password**","Chrisian Medrano");
 		
-		test.Authentication("cmedrano6", "cm6693916@gmail.com","123Password**", "Christian Medrano", "1");
+		test4.Authentication("cmedrano6", "cm6693916@gmail.com","123Password**", "Christian Medrano", "1");
 		
-		test.UserAuth.login("cmedrano6", "123Password**");
-			
-		test.UserAuth.hash(test.UserInfo.getPassword());
-
-		System.out.printf("Testing hash key: %n" + test.UserAuth.getKey());
+		test4.UserInfo.login("cmedrano6", "123Password**");
 		
-		
-		System.out.printf("Testing hash table: %n" + test.UserAuth.getHashTable() + "\n");
+	
 		
 	}
 
